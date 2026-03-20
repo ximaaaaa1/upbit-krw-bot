7fdd60cf11dd2371ab2d5fc92020ee09e92bb071
ximaaaaa1 <maksim2020pol@gmail.com>
2026-03-20 04:00:47 +0200

    Add CORS support for cross-origin requests

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(express.static(__dirname));
 
